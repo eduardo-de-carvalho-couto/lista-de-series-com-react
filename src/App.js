b2d6d9893fb33526201027462dffa2965cf2338f
Eduardo <coutoeduz@gmail.com>
2022-12-23 20:30:42 -0300

    fazendo o submit de series

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario'
 
 function App() {
+  
+  const [series, setSeries] = useState([])
+
+  const aoCadastrarNovaSerie = serie => {
+    console.log(serie)
+    setSeries(...series, serie)
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoCadastrarSerie={serie => aoCadastrarNovaSerie(serie)} />
     </div>
   );
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,24 +1,55 @@
+import { useState } from 'react'
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
-    const series = [
+    const servicos = [
         'Netflix',
         'Prime Video',
         'HBO Max',
         'Showtime'
     ]
 
+    const [nome, setNome] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [servico, setServico] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoCadastrarSerie({
+            nome: nome, 
+            imagem: imagem, 
+            servico: servico
+        })
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para cadastrar uma nova série</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Serie" itens={series}/>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome" 
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem" 
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Servico" 
+                    itens={servicos}
+                    valor={servico}
+                    aoAlterado={valor => setServico(valor)}
+                />
                 <Botao>
                     Adicionar Série
                 </Botao>
